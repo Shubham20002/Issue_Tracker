d4573ddaa6209965e08ccd94d9cf250a866477a2
shubham <shubham992284@gmail.com>
2023-07-21 22:19:21 +0530

    project id passed to issueform

--- controllers/issuecontroller.js
+++ controllers/issuecontroller.js
@@ -2,11 +2,14 @@ const Project=require('../models/project');
 const Issue=require("../models/issue");
 
 module.exports.issueform=function(req,res){
+    const project=req.params.id;
     return res.render('createissue',{
-        
+        project_id:project
+
     })
 }
 module.exports.createissue= async function(req,res){
+    
     try{
         const issue= await Issue.create(req.body);
     return res.redirect('back');
--- routes/issue.js
+++ routes/issue.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 const issuecontroller=require('../controllers/issuecontroller');
 
-router.get('/createissue',issuecontroller.issueform);
+router.get('/createissue/:id',issuecontroller.issueform);
 router.post('/addissue',issuecontroller.createissue);
 
 module.exports=router;
